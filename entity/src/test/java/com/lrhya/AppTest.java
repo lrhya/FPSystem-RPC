c4c1457d2109fca24df925e372b02e10b6cdcb66
aLiruihao <1262946752@qq.com>
2019-03-27 10:49:37 +0800

    基本完成entity层编码

---
+++ entity/src/test/java/com/lrhya/AppTest.java
@@ -0,0 +1,20 @@
+package com.lrhya;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple Product.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
